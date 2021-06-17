adf98d7fe04f23049a09b61ff66b40b70362ad95
davidcrowell <dcrowell@roofscope.com>
2021-06-17 09:16:16 -0600

    New Footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Super Simple Fast`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `No Logins, One small ad. Basic information Simple and Fast`,
     author: `cogear`,
   },
   plugins: [
